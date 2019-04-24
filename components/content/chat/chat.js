8a2cabf1edf1f995485fdf77f57620408a04e5a6
m84013885 <84013885@qq.com>
2019-04-24 19:25:39 +0800

    fix : 提交

--- components/content/chat/chat.js
+++ components/content/chat/chat.js
@@ -7,17 +7,45 @@ Component({
     },
     content: {
       type: String,
-      value: '内容',
+      value: '90182390718939012890$%@#q!说委屈w_eq.w e——ertrwqe',
     },
+    mine: {
+      type: Boolean,
+      value: false
+    }
   },
   data: {
     // 这里是一些组件内部数据
-    getTime: '0秒前'
+    width: '526rpx'
   },
-  attached: function() {
-
+  attached: function () {
+    this.contentWidth()
   },
   methods: {
+    contentWidth() {
+      // const query = wx.createSelectorQuery().in(this)
+      // query.select('#textwidth').fields({
+      //   size: true
+      // },function(res){
+      //   console.log(res)
+      // }).exec()
+      // var dom = wx.createIntersectionObserver(this)
+      // const a = IntersectionObserver.relativeTo('#textwidth')
+      // console.log(a)
+      // wx.createSelectorQuery().select('#textwidth').boundingClientRect(function(res){
+      //   console.log(res) // 这个组件内 #the-id 节点的上边界坐标
+      // }).exec()
+      // const content = this.data.content
+      // const texts = content.split('')
+      // var query = wx.createSelectorQuery().in(this)
+      // console.log(query)
+      // const width= query.select('#textwidth').boundingClientRect(function(rect){
+      //   console.log(rect)   // 节点的宽度
+      // }).exec()
+      // console.log(width)
+      // const textsWidth = texts.map((content,index)=>{
 
+      // })
+    }
   }
 })
